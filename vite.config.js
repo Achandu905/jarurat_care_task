1e249246776ad4bd90ea36a99ddedd1e7047b5db
unknown <ajmeerachandu2001@gmail.com>
2024-08-15 22:57:31 +0530

    setup gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:jarurat_care_task,
   plugins: [react()],
 })
